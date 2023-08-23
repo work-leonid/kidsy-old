3b6f5f15fc723aa6334623263be80fbf45fd99c5
Leonid Nazarov <work.leonid@gmail.com>
2023-08-23 20:01:46 +0400

    add today button and open browser after server starts

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,7 @@
+require_relative "../lib/puma/plugin/open"
+plugin :open
 # Puma can serve each request in a thread from an internal thread pool.
+#
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
